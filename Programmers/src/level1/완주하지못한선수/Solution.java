16ceb7657a0306adf6180fcf3fac4ad3c2406ff1
eunyooung <choi327391@gmail.com>
2022-05-11 23:13:18 +0900

    feat: Add hash solution
    Count the participants and take off the completion.
    Then find the key(person) with value 1 in the hashmap.

--- Programmers/src/level1/완주하지못한선수/Solution.java
+++ Programmers/src/level1/완주하지못한선수/Solution.java
@@ -3,11 +3,12 @@ package level1.완주하지못한선수;
 import java.util.*;
 
 class Solution {
+    
     public String solution(String[] participant, String[] completion) {
         String answer = "";
         Arrays.sort(participant);
         Arrays.sort(completion);
-        
+
         int i = 0;
         for (i = 0; i < completion.length; i++) {
             if (!participant[i].equals(completion[i])) {
@@ -17,6 +18,24 @@ class Solution {
         }
         if (answer.isEmpty()) {
             answer = participant[i];
+        }
+        return answer;
+    }
+
+    public String solution_hash(String[] participant, String[] completion) {
+        String answer = "";
+        Map<String, Integer> map = new HashMap<>();
+        for (String player : participant)
+            map.put(player, map.getOrDefault(player, 0) + 1);
+        for (String player : completion)
+            map.put(player, map.get(player) - 1);
+        Iterator<Map.Entry<String, Integer>> iter = map.entrySet().iterator();
+        while (iter.hasNext()) {
+            Map.Entry<String, Integer> entry = iter.next();
+            if (entry.getValue() != 0) {
+                answer = entry.getKey();
+                break;
+            }
         }
         return answer;
     }
