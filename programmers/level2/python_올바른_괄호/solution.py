bc085113f267dd2facde9fd2cce7fa5bfd2900a3
eunyooung <choi327391@gmail.com>
2023-06-21 15:48:13 +0900

    Add solution 2023-06-21

---
+++ programmers/level2/python_올바른_괄호/solution.py
@@ -0,0 +1,15 @@
+def solution(s):
+    
+    stack = []
+    
+    for i in s:    
+        if i == "(" :
+            stack.append(i)
+        elif i == ")" and len(stack) != 0 :
+            stack.pop()
+        else:
+            return False
+    
+    if len(stack) == 0 :
+        return True
+    return False
