9c66c8b840bdad7577522c4bebe05c1a6790516f
eunyooung <choi327391@gmail.com>
2023-07-09 02:08:05 +0900

    Add solution 2023-07-09

---
+++ programmers/level1/java_평균_구하기/Solution.java
@@ -0,0 +1,15 @@
+package level1.java_평균_구하기;
+
+class Solution {
+    
+    public double solution(int[] arr) {
+        
+        double answer = 0;
+        
+        for (int i = 0; i < arr.length; i++) {
+            answer += arr[i];
+        }
+        
+        return answer / arr.length;
+    }
+}
