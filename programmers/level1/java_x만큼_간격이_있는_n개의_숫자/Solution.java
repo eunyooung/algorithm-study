2957b96c4a2f6e45d82c55f9d0844a9ef0bfc01e
eunyooung <choi327391@gmail.com>
2023-08-12 23:23:53 +0900

    Add solution 2023-08-12

---
+++ programmers/level1/java_x만큼_간격이_있는_n개의_숫자/Solution.java
@@ -0,0 +1,18 @@
+package level1.java_x만큼_간격이_있는_n개의_숫자;
+
+class Solution {
+    
+    public long[] solution(int x, int n) {
+        
+        long[] answer = new long[n];
+        long num = x;
+        
+        for (int i = 0; i < n; i ++) {
+            
+            answer[i] = num;
+            num += x;
+        }
+        
+        return answer;
+    }
+}
