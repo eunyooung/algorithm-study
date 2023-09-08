d38155d935f2f2a7489ff180e6e1f65fa4d0365b
eunyooung <choi327391@gmail.com>
2023-09-08 14:54:49 +0900

    Add solution 2023-09-08

---
+++ programmers/level1/java_기사단원의_무기/Solution.java
@@ -0,0 +1,31 @@
+package level1.java_기사단원의_무기;
+
+class Solution {
+    
+    public int solution(int number, int limit, int power) {
+        
+        int answer = 0;
+        
+        for (int i = 1; i <= number; i++) {
+            
+            int count = 0;
+            
+            for (int j = 1; j * j <= i; j++) {
+                
+                if (j * j == i) {
+                    count++;
+                } else if (i % j == 0) {
+                    count += 2;
+                }
+            }
+            
+            if (count > limit) {
+                answer += power;
+            } else {
+                answer += count;
+            }
+        }
+        
+        return answer;
+    }
+}
