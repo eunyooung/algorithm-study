c052a567369cd17e262d75252bdbd07f575fd257
eunyooung <choi327391@gmail.com>
2023-09-18 21:52:45 +0900

    Add solution 2023-09-18

---
+++ programmers/level2/java_n2_배열_자르기/Solution.java
@@ -0,0 +1,18 @@
+package level2.java_n2_배열_자르기;
+
+class Solution {
+    
+    public int[] solution(int n, long left, long right) {
+        
+        int[] answer = new int [(int)(right - left) + 1];
+        
+        for (int i = 0; i < answer.length; i++) {
+            
+            int x = (int)(left % n + 1);
+            int y = (int)(left / n + 1);
+            left++;
+            answer[i] = Math.max(x, y);
+        }
+        return answer;
+    }
+}
