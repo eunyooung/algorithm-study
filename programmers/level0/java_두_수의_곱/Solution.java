25bf6a96f889d6cc23ef3ff0705bd4dcc24ba9ed
eunyooung <choi327391@gmail.com>
2023-10-10 19:21:42 +0900

    Add solution 2023-10-10

---
+++ programmers/level0/java_두_수의_곱/Solution.java
@@ -0,0 +1,9 @@
+package level0.java_두_수의_곱;
+
+class Solution {
+    
+    public int solution(int num1, int num2) {
+        
+        return num1 * num2;
+    }
+}
---
+++ programmers/level0/java_두_수의_차/Solution.java
@@ -0,0 +1,9 @@
+package level0.java_두_수의_차;
+
+class Solution {
+    
+    public int solution(int num1, int num2) {
+        
+        return num1 - num2;
+    }
+}
