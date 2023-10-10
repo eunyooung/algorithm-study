20604c29ff09930cb14610cbd7da421bb14524eb
eunyooung <choi327391@gmail.com>
2023-10-11 00:18:55 +0900

    Add solution 2023-10-11

---
+++ programmers/level0/java_나머지_구하기/Solution.java
@@ -0,0 +1,9 @@
+package level0.java_나머지_구하기;
+
+class Solution {
+    
+    public int solution(int num1, int num2) {
+        
+        return num1 % num2;
+    }
+}
---
+++ programmers/level0/java_나이_출력/Solution.java
@@ -0,0 +1,9 @@
+package level0.java_나이_출력;
+
+class Solution {
+    
+    public int solution(int age) {
+        
+        return 2022 - age + 1;
+    }
+}
