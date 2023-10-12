33142fdcac768ea66379806ef1d1060cbc4a5d4a
eunyooung <choi327391@gmail.com>
2023-10-12 17:33:24 +0900

    Add solution 2023-10-12

---
+++ programmers/level0/java_몫_구하기/Solution.java
@@ -0,0 +1,9 @@
+package level0.java_몫_구하기;
+
+class Solution {
+    
+    public int solution(int num1, int num2) {
+        
+        return num2 == 0 ? 0 : num1 / num2;
+    }
+}
---
+++ programmers/level0/java_숫자_비교하기/Solution.java
@@ -0,0 +1,9 @@
+package level0.java_숫자_비교하기;
+
+class Solution {
+    
+    public int solution(int num1, int num2) {
+        
+        return num1 == num2 ? 1 : -1;
+    }
+}
