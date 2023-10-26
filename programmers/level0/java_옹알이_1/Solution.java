989f2b96ac3aad4e56589c17498115056d0b10b5
eunyooung <choi327391@gmail.com>
2023-10-26 18:01:42 +0900

    Add solution 2023-10-26

---
+++ programmers/level0/java_옹알이_1/Solution.java
@@ -0,0 +1,19 @@
+package level0.java_옹알이_1;
+
+class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+        for (int i = 0; i < babbling.length; i++) {
+            babbling[i] = babbling[i].replaceFirst("aya", "1");
+            babbling[i] = babbling[i].replaceFirst("ye", "1");
+            babbling[i] = babbling[i].replaceFirst("woo", "1");
+            babbling[i] = babbling[i].replaceFirst("ma", "1");
+            babbling[i] = babbling[i].replace("1", "");
+            
+            if (babbling[i].isEmpty()) {
+                answer += 1;
+            }
+        }
+        return answer;
+    }
+}
