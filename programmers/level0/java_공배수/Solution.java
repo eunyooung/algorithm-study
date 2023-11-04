85000793721bd96e97f5b6d29975cd8835a650ac
eunyooung <choi327391@gmail.com>
2023-11-04 15:42:09 +0900

    Add solution 2023-11-04

---
+++ programmers/level0/java_공배수/Solution.java
@@ -0,0 +1,9 @@
+package level0.java_공배수;
+
+class Solution {
+    
+    public int solution(int number, int n, int m) {
+        
+        return number % n == 0 && number % m == 0 ? 1 : 0;
+    }
+}
---
+++ programmers/level0/java_정수_부분/Solution.java
@@ -0,0 +1,9 @@
+package level0.java_정수_부분;
+
+class Solution {
+    
+    public int solution(double flo) {
+        
+        return (int) flo;
+    }
+}
