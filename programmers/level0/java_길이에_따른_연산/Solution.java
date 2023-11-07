beff15f95726d8c9da88afc3fee4fe1434c61862
eunyooung <choi327391@gmail.com>
2023-11-07 23:10:12 +0900

    Add solution 2023-11-07

---
+++ programmers/level0/java_길이에_따른_연산/Solution.java
@@ -0,0 +1,26 @@
+package level0.java_길이에_따른_연산;
+
+class Solution {
+    
+    public int solution(int[] num_list) {
+        
+        int answer = 1;
+        
+        if (num_list.length <= 10) {
+            
+            for (int i = 0; i < num_list.length; i++) {
+                answer *= num_list[i];
+            }
+            
+        } else {
+            
+            for (int i = 0; i < num_list.length; i++) {
+                answer += num_list[i];
+            }
+            
+            answer -= 1;
+        }
+        
+        return answer;
+    }
+}
