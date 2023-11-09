5722ceb69b3836e68c539f0783a9277e92320e04
eunyooung <choi327391@gmail.com>
2023-11-09 21:41:39 +0900

    Add solution 2023-11-09

---
+++ programmers/level0/java_문자열_붙여서_출력하기/Solution.java
@@ -0,0 +1,13 @@
+package level0.java_문자열_붙여서_출력하기;
+
+import java.util.Scanner;
+
+public class Solution {
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.next() + sc.next());
+        sc.close();
+    }
+}
