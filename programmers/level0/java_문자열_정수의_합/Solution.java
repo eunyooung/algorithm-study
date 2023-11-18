c9bd264571ded81400b538d3f81cf4246d7b1066
eunyooung <choi327391@gmail.com>
2023-11-18 22:06:00 +0900

    Add solution 2023-11-18

---
+++ programmers/level0/java_문자열_정수의_합/Solution.java
@@ -0,0 +1,16 @@
+package level0.java_문자열_정수의_합;
+
+class Solution {
+    
+    public int solution(String num_str) {
+        
+        int answer = 0;
+        
+        for (int i = 0; i < num_str.length(); i++) {
+            
+            answer += Character.getNumericValue(num_str.charAt(i));
+        }
+        
+        return answer;
+    }
+}
