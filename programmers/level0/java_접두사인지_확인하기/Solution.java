33f6174a3af09c533b4d5f2c813e91036a583f92
eunyooung <choi327391@gmail.com>
2023-11-22 18:28:25 +0900

    Add solution 2023-11-22

---
+++ programmers/level0/java_접두사인지_확인하기/Solution.java
@@ -0,0 +1,9 @@
+package level0.java_접두사인지_확인하기;
+
+class Solution {
+    
+    public int solution(String my_string, String is_prefix) {
+        
+        return my_string.startsWith(is_prefix) ? 1 : 0;
+    }
+}
