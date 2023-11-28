bbf501f1b9b6d86b09f8e97e448625ca5ccc5cf1
eunyooung <choi327391@gmail.com>
2023-11-28 21:41:14 +0900

    Add solution 2023-11-28

---
+++ programmers/level0/java_이어_붙인_수/Solution.java
@@ -0,0 +1,21 @@
+package level0.java_이어_붙인_수;
+
+class Solution {
+    
+    public int solution(int[] num_list) {
+        
+        String even = "";
+        String odd = "";
+        
+        for (int i = 0; i < num_list.length; i++) {
+            
+            if (num_list[i] % 2 == 0) {
+                even += Integer.toString(num_list[i]);
+            } else {
+                odd += Integer.toString(num_list[i]);
+            }
+        }
+        
+        return Integer.parseInt(even) + Integer.parseInt(odd);
+    }
+}
