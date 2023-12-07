ab156cee7b8d47eea626b90bd9dc4e0e6aba5703
eunyooung <choi327391@gmail.com>
2023-12-07 15:55:11 +0900

    Add solution 2023-12-07

---
+++ programmers/level0/java_n_번째_원소부터/Solution.java
@@ -0,0 +1,16 @@
+package level0.java_n_번째_원소부터;
+
+class Solution {
+    
+    public int[] solution(int[] num_list, int n) {
+        
+        int[] answer = new int[num_list.length - n + 1];
+        
+        for (int i = 0; i < answer.length; i ++) {
+            
+            answer[i] = num_list[i + n - 1];
+        }
+        
+        return answer;
+    }
+}
