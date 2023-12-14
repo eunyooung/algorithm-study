485235b9d1b52d32f44fe5010b620bc5e158287d
eunyooung <choi327391@gmail.com>
2023-12-14 22:14:32 +0900

    Add solution 2023-12-14

---
+++ programmers/level0/java_배열에서_문자열_대소문자_변환하기/Solution.java
@@ -0,0 +1,22 @@
+package level0.java_배열에서_문자열_대소문자_변환하기;
+
+class Solution {
+    
+    public String[] solution(String[] strArr) {
+        
+        String[] answer = new String[strArr.length];
+        
+        for (int i = 0; i < strArr.length; i++) {
+            
+            if (i % 2 == 0) {
+                
+                answer[i] = strArr[i].toLowerCase();
+            } else {
+                
+                answer[i] = strArr[i].toUpperCase();
+            }
+        
+        }
+        return answer;
+    }
+}
