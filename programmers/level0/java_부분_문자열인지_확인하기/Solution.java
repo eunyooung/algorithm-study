ccd0ee6bb119966b0115e37749963cb7b7455828
eunyooung <choi327391@gmail.com>
2023-12-15 17:22:54 +0900

    Add solution 2023-12-15

---
+++ programmers/level0/java_부분_문자열인지_확인하기/Solution.java
@@ -0,0 +1,9 @@
+package level0.java_부분_문자열인지_확인하기;
+
+class Solution {
+    
+    public int solution(String my_string, String target) {
+        
+        return my_string.contains(target) ? 1 : 0;
+    }
+}
