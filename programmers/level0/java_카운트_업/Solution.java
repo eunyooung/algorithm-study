53cbdd6236417c65edb9864a1e2cc6ae63e2724c
eunyooung <choi327391@gmail.com>
2023-12-25 23:03:04 +0900

    Add solution 2023-12-25

---
+++ programmers/level0/java_카운트_업/Solution.java
@@ -0,0 +1,18 @@
+package level0.java_카운트_업;
+
+class Solution {
+    
+    public int[] solution(int start_num, int end_num) {
+        
+        int[] answer = new int[end_num - start_num + 1];
+        
+        int index = 0;
+        
+        for (int i = start_num; i <= end_num; i++) {
+            
+            answer[index++] = i;
+        }
+        
+        return answer;
+    }
+}
