54afcb6109e9eb98ca1062afa96ddd092f88084f
eunyooung <choi327391@gmail.com>
2024-01-01 21:44:30 +0900

    Add solution 2024-01-01

---
+++ programmers/level0/java_마지막_두_원소/Solution.java
@@ -0,0 +1,23 @@
+package level0.java_마지막_두_원소;
+
+class Solution {
+    
+    public int[] solution(int[] num_list) {
+    
+        int n = num_list.length;
+        int[] answer = new int[n + 1];
+    
+        for (int i = 0; i < n; i++) {
+            
+            answer[i] = num_list[i];
+        }
+        
+        if (answer[n - 1] > answer[n - 2]) {
+            answer[n] =  answer[n - 1] - answer[n - 2];
+        } else {
+            answer[n] = answer[n - 1] * 2;
+        }
+        
+        return answer;
+    }
+}
