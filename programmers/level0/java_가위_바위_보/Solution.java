a629354d9a4b507efac198c6138aff257d17df0e
eunyooung <choi327391@gmail.com>
2024-01-06 23:47:51 +0900

    Add solution 2024-01-06

---
+++ programmers/level0/java_가위_바위_보/Solution.java
@@ -0,0 +1,22 @@
+package level0.java_가위_바위_보;
+
+class Solution {
+    
+    public String solution(String rsp) {
+        
+        String answer = "";
+        
+        for (int i = 0; i < rsp.length(); i++) {
+            
+            if (rsp.charAt(i) == '2') {
+                answer += "0";
+            } else if (rsp.charAt(i) == '0') {
+                answer += "5";
+            } else {
+                answer += "2";
+            }
+        }
+        
+        return answer;
+    }
+}
