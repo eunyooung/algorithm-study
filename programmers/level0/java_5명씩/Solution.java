59dbd24ec2a82d26285921bb52574fad1e744266
eunyooung <choi327391@gmail.com>
2024-01-08 17:51:28 +0900

    Add solution 2024-01-08

---
+++ programmers/level0/java_5명씩/Solution.java
@@ -0,0 +1,18 @@
+package level0.java_5명씩;
+
+import java.util.*;
+
+class Solution {
+    
+    public String[] solution(String[] names) {
+        
+        List<String> list = new ArrayList<>();
+        
+        for (int i = 0; i < names.length; i += 5) {
+            
+            list.add(names[i]);
+        }
+        
+        return list.toArray(new String[0]);
+    }
+}
