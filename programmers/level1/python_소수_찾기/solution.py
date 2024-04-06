571a4659b03e2b205f9b0683ca335088c9767f74
eunyooung <choi327391@gmail.com>
2024-04-06 21:46:35 +0900

    Add solution 2024-04-06

---
+++ programmers/level1/python_소수_찾기/solution.py
@@ -0,0 +1,16 @@
+import math
+
+def solution(n):
+    
+    answer = 0
+    
+    for i in range(2, n + 1) :
+        flag = True
+        for j in range(2, int(math.sqrt(i)) + 1) :
+            if i % j == 0 :
+                flag = False
+                break
+        if flag == True :
+            answer += 1
+    
+    return answer
